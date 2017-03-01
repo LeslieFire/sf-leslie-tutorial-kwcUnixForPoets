3ac5c1730942f270a1468bcdd325f13f77b8d607
Leslie <249893977@qq.com>
2017-03-01 14:04:26 +0800

    grep

--- kwc-unix-for-poets.sh
+++ kwc-unix-for-poets.sh
@@ -19,4 +19,30 @@ tail -n +3 data/genesis.words > data/genesis.nextnextwords
 paste data/genesis.words data/genesis.nextwords data/genesis.nextnextwords | sort | uniq -c | sort -nr > data/genesis.trigrams
 
 
+# count uppercase words
+egrep -c '^[A-Z]+$' data/genesis.words
+# count lowercase words
+egrep '^[a-z]+$' data/genesis.words | wc -l
+# count 4-letter words
+egrep -c '^[A-Za-z]{4}$' data/genesis.words
+# count words with no vowels
+grep -v '[AEIOUaeiou]' data/genesis.words
+# 1-syllable words
+grep  '^[^aeiou]*[aeiou][^aeiou]*$' data/genesis.words
+# 2-syllable words
+grep  '^[^aeiou]*[aeiou][^aeiou]*[aeiou][^aeiou]*$' data/genesis.words
+grep -Ei "^[^aeiouy]*[aeiouy][^aeiouy]*[aeiouy][^aeiouy]*$" data/genesis.words | sort -f | uniq -i > data/genesis.2syllables
+
+# delete word ending with silent "2" or containing dipthongs
+grep -Eiv "(ow|ou|ie|oi|oo|ea|ee|ai|[aeiou]y).*|[aeiouy][^aeiouy]e$" data/genesis.2syllables
+
+# find verses with word "light"
+grep -Ei  '^(.*vi.*)$' data/genesis.words
+# 2 or more instances of "light"
+grep -Ei  '^(.*vi.*){2,}$' data/genesis.words | wc -l
+# 3 or more instances of "light"
+grep -Ei  '^(.*vi.*){3,}$' data/genesis.words | wc -l
+
+
+
 
