9ae0c89f9fe9fe0aa9dc857be7b850b8c769114a
Leslie <249893977@qq.com>
2017-03-01 12:14:18 +0800

    trigrams

--- kwc-unix-for-poets.sh
+++ kwc-unix-for-poets.sh
@@ -11,3 +11,12 @@ tr -sc 'A-Za-z' '\012' < data/words.txt | sort | uniq -c | sort -d -k2
 
 # rhyming order
 tr -sc 'A-Za-z' '\012' < data/words.txt | sort | uniq -c | rev | sort -d | rev
+
+# count trigrams
+tr -sc 'A-Za-z' '\012' < data/words.txt > data/genesis.words
+tail -n +2 data/genesis.words > data/genesis.nextwords
+tail -n +3 data/genesis.words > data/genesis.nextnextwords
+paste data/genesis.words data/genesis.nextwords data/genesis.nextnextwords | sort | uniq -c | sort -nr > data/genesis.trigrams
+
+
+
